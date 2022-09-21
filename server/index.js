427600010a9a74eda8ad920f68318f4fb7a6c963
Timothy Loo <TimothyLoo07@gmail.com>
2022-09-21 07:19:21 -1000

    create starting app

---
+++ server/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}`));
