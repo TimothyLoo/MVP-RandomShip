9f5d5b9db909ae4785da8480f1e642d22c3d3e22
Timothy Loo <TimothyLoo07@gmail.com>
2022-09-23 17:16:52 -1000

    update

--- client/src/StartPage.jsx
+++ client/src/StartPage.jsx
@@ -1,9 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
 import { crashed } from './stars.js';
 import { GameState } from './App.jsx';
 
 export default function StartPage() {
+  const [topScores, setTopScores] = useState([]);
+
   const setAppSt = useContext(GameState);
+
+  useEffect(() => {
+    axios
+      .get('/topScores')
+      .then(({ data }) => setTopScores(data))
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className='start'>
@@ -14,6 +24,13 @@ export default function StartPage() {
         }}>
         Start Game
       </button>
+      <br />
+      <u>
+        <b>Top Scores</b>
+      </u>
+      {topScores.map((score) => (
+        <div>{score.score}</div>
+      ))}
     </div>
   );
 }
